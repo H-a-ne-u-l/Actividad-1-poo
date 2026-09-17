58088c9111f79335682c1cab7ea289fb6ea8ed83
Haneul <yuranitatiana2006@gmail.com>
2026-09-16 22:18:10 -0500

    Update ejercicio5.py

--- ejercicio-2/ejercicio5.py
+++ ejercicio-2/ejercicio5.py
@@ -1,1 +1,32 @@
+class Seguimiento:
 
+    @staticmethod
+    def calcular_primera_suma(suma: float, x: float) -> float:
+        return suma + x
+
+    @staticmethod
+    def calcular_nuevo_x(x: float, y: float) -> float:
+        return x + (y ** 2)
+
+    @staticmethod
+    def calcular_suma_final(suma: float, x: float, y: float) -> float:
+        return suma + (x / y)
+
+
+def main():
+    suma = 0.0
+    x = 20.0
+
+    suma = Seguimiento.calcular_primera_suma(suma, x)
+
+    y = 40.0
+
+    x = Seguimiento.calcular_nuevo_x(x, y)
+
+    suma = Seguimiento.calcular_suma_final(suma, x, y)
+
+    print(f"EL VALOR DE LA SUMA ES: {suma}")
+
+
+if __name__ == "__main__":
+    main()
